b084918a65213c89ba4e1dac91dc4ed9e08412bc
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-15 18:47:50 -0300

    feat: create table Servico_Animal

--- petshop.sql
+++ petshop.sql
@@ -25,3 +25,17 @@ CREATE TABLE Funcionario(
     funcao VARCHAR(200) NOT NULL 
 ); 
 
+CREATE TABLE Servico_Animal( 
+    codigo_servico INT AUTO_INCREMENT PRIMARY KEY, 
+    CPF_cliente VARCHAR(11) NOT NULL, 
+    CPF_funcionario VARCHAR(11) NOT NULL, 
+    codigo_animal INT NOT NULL, 
+    descricao_servico VARCHAR(100) NOT NULL, 
+    preco DECIMAL(10,2) NOT NULL, 
+    data_servico DATE NOT NULL, 
+
+    FOREIGN KEY (CPF_cliente) REFERENCES Cliente(CPF) ON DELETE CASCADE,  
+    FOREIGN KEY (CPF_funcionario) REFERENCES Funcionario(CPF) ON DELETE CASCADE,  
+    FOREIGN KEY (codigo_animal) REFERENCES Animal(Codigo) ON DELETE CASCADE 
+); 
+
