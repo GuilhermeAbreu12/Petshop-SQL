df1a1dc982558caf4a49416d2bed5d35cc731206
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-15 18:47:14 -0300

    feat: create table Cliente

--- petshop.sql
+++ petshop.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE petshop;
 USE petshop;  
 
+CREATE TABLE Cliente( 
+    CPF VARCHAR(11) PRIMARY KEY NOT NULL, 
+    nome VARCHAR(100) NOT NULL, 
+    telefone VARCHAR(12) NOT NULL, 
+    endereco TEXT NOT NULL 
+); 
+
